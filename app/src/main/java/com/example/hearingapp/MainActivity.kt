7c322a40dfbf117ba407c9964e0309173a5d9f0a
narotam26 <nsv261102@gmail.com>
2022-06-07 22:34:22 +0530

    binarysearch

--- app/src/main/java/com/example/hearingapp/MainActivity.kt
+++ app/src/main/java/com/example/hearingapp/MainActivity.kt
@@ -6,16 +6,23 @@ import android.media.AudioTrack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.example.hearingapp.databinding.ActivityMainBinding
 import kotlin.experimental.and
+import kotlin.math.floor
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 class MainActivity : AppCompatActivity() {
-    val duration: Int = 10 // seconds
-    val sampleRate: Int = 10000
+    private lateinit var binding: ActivityMainBinding
+    val duration: Int = 6 // seconds
+    val sampleRate: Int = 12000
     val numSamples: Int = duration * sampleRate
     val sample = DoubleArray(numSamples)
-
-    //private final double sample[] = new double[numSamples];
-    val freqOfTone: Double = 125.0 // hz
+    var start: Double = 125.0
+    var end: Double = 8000.0
+    var flag: Boolean = true
+    var freqOfTone: Double = 125.0 // hz
 
     val generatedSnd = ByteArray(2 * numSamples)
 
@@ -23,32 +30,78 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    }
+        var minfreq:Double = end
 
-    override fun onResume() {
-        super.onResume()
-
-        // Use a new tread as this can take a while
-        val thread: Thread = Thread(Runnable() {
+            val thread: Thread = Thread(Runnable() {
             run() {
-                genTone()
-                handler.post(Runnable() {
+                while (start <= end) {
+                    if (flag) {
+                        binding.btnYes.isClickable = false
+                        binding.btnNo.isClickable = false
+                        var mid: Double = floor((start + end) / 2)
+                        flag=false
 
-                    run() {
-                        playSound();
+                        handler.post(Runnable() {
+                            binding.tvFreq.text =
+                                "Playing sound of freq " + mid.toString()
+                            run() {
+                                genTone(mid)
+                                Log.i("freq",mid.toString())
+                                playSound()
+                            }
+                            Handler().postDelayed({
+                                binding.btnYes.isClickable = true
+                                binding.btnNo.isClickable = true
+                                binding.btnYes.setOnClickListener {
+                                    minfreq = mid
+                                    end = mid - 10
+                                    flag=true
+                                }
+                                binding.btnNo.setOnClickListener {
+                                    start = mid + 10
+                                    flag=true
+                                }
+                            }, 3000)
+
+
+                        })
                     }
-                })
+                }
             }
-        });
-        thread.start();
-    }
+        })
+        thread.start()
 
-    private fun genTone() {
+                //flag=false
+            //}
+        }
+
+
+
+//    override fun onResume() {
+//        super.onResume()
+//
+//        // Use a new tread as this can take a while
+//        val thread: Thread = Thread(Runnable() {
+//            run() {
+//                genTone()
+//                handler.post(Runnable() {
+//
+//                    run() {
+//                        playSound()
+//                    }
+//                })
+//            }
+//        });
+//        thread.start();
+//    }
+
+    private fun genTone(freq:Double) {
         // fill out the array
         for (i in 0 until numSamples) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone))
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freq))
         }
 
         var idx: Int = 0
@@ -72,5 +125,6 @@ class MainActivity : AppCompatActivity() {
             AudioTrack.MODE_STATIC)
         audioTrack.write(generatedSnd, 0, generatedSnd.size)
         audioTrack.play()
+        //flag=true
     }
 }
