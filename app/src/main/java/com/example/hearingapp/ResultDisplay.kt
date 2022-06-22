760d0d2c8a9124719c53fbce0d2006a2c652d6c4
Prasant <77951529+prasant2603@users.noreply.github.com>
2022-06-22 08:32:59 +0530

    basicGraph

--- app/src/main/java/com/example/hearingapp/ResultDisplay.kt
+++ app/src/main/java/com/example/hearingapp/ResultDisplay.kt
@@ -1,9 +1,15 @@
 package com.example.hearingapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.hearingapp.databinding.ActivityResultDisplayBinding
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
+import java.security.KeyStore
 
 class ResultDisplay : AppCompatActivity() {
     private lateinit var binding: ActivityResultDisplayBinding
@@ -13,7 +19,18 @@ class ResultDisplay : AppCompatActivity() {
         setContentView(binding.root)
 
         val points: HashMap<String,String> = intent.getSerializableExtra("points") as HashMap<String, String>
-        binding.tvpoint.text= points.size.toString()
-
+        val linelist: ArrayList<Entry> = ArrayList()
+        for(i in points)
+        {
+            val x=i.key.toFloat()
+            val y=i.value.toFloat()
+            linelist.add(Entry(x,y))
+        }
+        val lineDataset=LineDataSet(linelist,"Count")
+        val linedata=LineData(lineDataset)
+        binding.graph.data=linedata
+        lineDataset.color=Color.BLACK
+        lineDataset.valueTextColor= Color.BLUE
+        lineDataset.valueTextSize=13f
     }
 }
