d075f750d018bcff008005d3fdbb81beda3cbc35
narotam26 <nsv261102@gmail.com>
2022-08-20 22:16:35 +0530

    icon

--- app/src/main/java/com/example/hearingapp/MainActivityLeft.kt
+++ app/src/main/java/com/example/hearingapp/MainActivityLeft.kt
@@ -43,7 +43,7 @@ class MainActivityLeft : AppCompatActivity() {
         setContentView(binding.root)
 
         var minfreq: Double = end
-        val frequencies = arrayListOf(125.0, 500.0, 2000.0, 8000.0)
+        val frequencies = arrayListOf(125.0, 500.0, 1000.0 , 2000.0, 8000.0)
         val dbs = arrayListOf(10, 20, 40, 60, 80)
         var freqi: Int = 0
         var dbi: Int = 0
